49366894ee4341a100946d915170b632c3b734d6
jralha <42118093+jralha@users.noreply.github.com>
2022-01-26 15:26:54 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from cv_calc import cv_calc
+import streamlit as st
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+import datetime
+
+# -- Set page config
+apptitle = 'Colateral Value Calculator'
+
+st.set_page_config(page_title=apptitle, page_icon=":eyeglasses:")
+
+#Header
+st.markdown("""
+**Collateral Value (CV) Calculator**
+
+Based on the following rules, below we can check out the CV at any given point in time.
+""")
+
+#Data Transforms
+st.image('.\misc\collateral_value_formula.png')
+
+data = pd.read_csv('data\cryptopunks_01-14-2022_13-55-22_downloaded.csv')
+
+cv_data_eth = cv_calc(data,'eth_price','utc_timestamp')['col_value']
+cv_data_usd = cv_calc(data,'usd_price','utc_timestamp')['col_value']
+
+out = cv_calc(data,'eth_price','utc_timestamp')[['eth_price','usd_price']]
+out['cv_eth'] = cv_calc(data,'eth_price','utc_timestamp')['col_value']
+out['cv_usd'] = cv_calc(data,'usd_price','utc_timestamp')['col_value']
+out.index.name = 'UTC Time'
+
+#Date filter
+st.markdown(''' **Visualizing CV Data** ''')
+
+start_date = st.date_input('Start Date:',value=datetime.datetime(2018,11,1))
+end_date = st.date_input('End Date:',value=datetime.datetime.now())
+
+mask = (out.index >= pd.to_datetime(start_date)) & (out.index <= pd.to_datetime(end_date))
+filter_data = out.loc[mask]
+
+#Plot
+st.text('')
+st.markdown(""" **Time plot**  """)
+
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+ax1.set_xlabel('Date')
+ax1.set_ylabel('CV (ETH)', color=color)
+ax1.plot(filter_data.index,filter_data['cv_eth'],color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+ax1.set_xticklabels(ax1.get_xticklabels(),rotation=45,ha='right',va='top')
+
+ax2 = ax1.twinx()
+color = 'tab:blue'
+ax2.set_ylabel('CV (USD)', color=color)
+ax2.plot(filter_data.index,filter_data['cv_usd'],color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()
+color = 'tab:green'
+ax2.set_ylabel('ETH Price (USD)', color=color)
+ax2.plot(filter_data.index,filter_data['usd_price']/filter_data['eth_price'],color=color,alpha=0.3)
+ax2.tick_params(axis='y', labelcolor=color, pad=75, width=0)
+
+fig.set_figheight(3)
+fig.set_figwidth(5)
+
+st.pyplot(fig)
+
+#Table
+st.dataframe(filter_data.dropna(),width=1000,height=400)
---
+++ cv_calc.py
@@ -0,0 +1,45 @@
+#%% Imports
+import pandas as pd
+import numpy as np
+
+#%% Define math for CV calculation
+def cv_calc(dataframe,price,timestamps):
+
+    '''Function that calculates the collateral value given prices and timestamps.
+    
+        Price is a parameter and can be set to eth or usd at runtime.
+        
+        Output is the input dataframe with the cv value as a new column.'''
+
+    #Copy dataset to a temp dataframe
+    df = dataframe.copy()
+
+    #Set time index
+    time_index = pd.to_datetime(df[timestamps])
+    df.index = time_index
+    df.drop(timestamps,axis=1)
+
+    #Remove 0 value transactions to not affect minimum threshold values
+    df = df.loc[df['eth_price'] > 0]
+
+    #Calculate the daily minimum threshold for prices
+    price_threshold = 0.15*df[price].resample('1D').quantile(0.25)
+    price_threshold.name = 'price_threshold'
+    
+    #Merge the daily thresholds onto the temp dataset
+    df = pd.merge(df,price_threshold,'left',left_index=True,right_on=timestamps)
+    
+    #Filter transactions under the threshold
+    filtered = df.loc[df[price] > df['price_threshold']]
+
+    #Calculate the CV value as a 30 day moving average of the daily minimum filtered transaction price.
+    #Also shifts the time series 1 day forward to only get past data.
+    cv = filtered[price].resample('1D').min().rolling('30D').mean().shift(1,'D')
+    cv.name = 'col_value'
+
+    #Merges daily CV values back onto the main dataset for exporting
+    out = pd.merge(filtered,cv,'left',left_index=True,right_on=timestamps)
+    out.drop('price_threshold',axis=1)
+    
+    return out
+
